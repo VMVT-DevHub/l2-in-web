d3cad1456298ba9eb8b43068f7186426d981ff89
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2025-03-31 08:45:01 +0300

    validation for pattern

--- src/renderers/CombinedInput.tsx
+++ src/renderers/CombinedInput.tsx
@@ -27,8 +27,19 @@ export const CombinedInput = ({
   };
 
   const isNumeric = schemaProperties[inputKey].type === 'number';
+  const hasPattern = schemaProperties[inputKey].pattern;
 
   const handleValue = (val) => {
+    //allow user to clear input. Test regex pattern only if user types something
+    if (val.input && (data?.[inputKey]?.toString().length || 0) < val.input.length) {
+      if (hasPattern && val.input) {
+        const regex = new RegExp(hasPattern);
+        if (!regex.test(val.input)) {
+          return;
+        }
+      }
+    }
+
     const newValue = {
       [inputKey]: val.input ? (isNumeric ? Number(val.input) : val.input) : undefined,
       [optionKey]: val.option || undefined,
